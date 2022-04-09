5c38b173185453b0ac209ea019a691b64ca3b884
Lech Różański <l.rozanski88@gmail.com>
2022-04-09 20:44:21 +0200

    Improved grid

--- Assets/Scripts/TilePainter.cs
+++ Assets/Scripts/TilePainter.cs
@@ -13,13 +13,23 @@ public class TilePainter : SingletonMonoBehaviour<TilePainter> {
     [SerializeField, AssetsOnly, AssetSelector(Paths = "Assets/Tiles")]
     private Ditch ditchTile;
 
+    [SerializeField, SceneObjectsOnly]
+    private GameObject gridUI;
+
+    [SerializeField, SceneObjectsOnly]
+    private GameObject tileSelection;
+
     private void Update() {
         var mouseWorldPosition = MouseUtils.WorldPosition;
         var cell = tilemap.WorldToCell(mouseWorldPosition);
 
-        if (!tilemap.HasTile(cell)) {
+        var hasTile = tilemap.HasTile(cell);
+        gridUI.SetActive(hasTile);
+
+        if (!hasTile) {
             return;
         }
+        tileSelection.transform.position = tilemap.GetCellCenterWorld(cell);
         var tile = tilemap.GetTile<Tile>(cell);
 
         if (tile is Ditch || !Mouse.current.leftButton.wasPressedThisFrame) {
